8e1321704b2146b960bfe630cb89cf82249c8b1a
MinhNK251 <nguyenkhanhminh251@gmail.com>
2024-06-12 14:10:40 +0700

    Delete be/LactoBloom directory

--- be/LactoBloom/src/main/java/com/lactobloom/repository/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.lactobloom.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.lactobloom.model.Role;
-
-public interface RoleRepository extends JpaRepository<Role, Integer> {
-
-}
--- be/LactoBloom/src/main/java/com/lactobloom/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lactobloom.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.lactobloom.model.User;
-import java.util.List;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-    List<User> findByFullNameContaining(String substring);
-}
