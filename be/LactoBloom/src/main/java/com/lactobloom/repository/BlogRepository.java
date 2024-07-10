309faace04f8fa44f22a3b0cb38986a172187bdf
nguyenanhquan04 <81096510+nguyenanhquan04@users.noreply.github.com>
2024-07-10 08:51:28 +0700

    Delete be directory

--- be/LactoBloom/src/main/java/com/lactobloom/repository/BlogRepository.java
+++
@@ -1,8 +0,0 @@
-package com.lactobloom.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.lactobloom.model.Blog;
-
-public interface BlogRepository extends JpaRepository<Blog, Integer> {
-
-}
